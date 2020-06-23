0a7a74f7a29d07d44415b4cdd2b420b9ef0d886f
Joeri Van Hoof <jvanhoof@fortinet.com>
2020-06-23 12:03:06 +0200

    Update formatting

--- 01-variable.tf
+++ 01-variable.tf
@@ -43,7 +43,7 @@ terraform {
 # Terraform Microsoft Azure Provider
 ##############################################################################################################
 provider "azurerm" {
-  version = ">= 2.0.0"
+  version         = ">= 2.0.0"
   subscription_id = var.AZURE_SUBSCRIPTION_ID
   client_id       = var.AZURE_CLIENT_ID
   client_secret   = var.AZURE_CLIENT_SECRET
