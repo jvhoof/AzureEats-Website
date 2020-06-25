3f4e050e454d099e0c3421bc37525f1869b8b898
Joeri Van Hoof <jvanhoof@fortinet.com>
2020-06-25 09:12:25 +0200

    Update name SQL FW

--- 02-sqldb.tf
+++ 02-sqldb.tf
@@ -35,7 +35,7 @@ resource "azurerm_sql_database" "sqldatabase" {
 
 # Allow Azure services to acccess the Azure SQL DB
 resource "azurerm_sql_firewall_rule" "sqlfirewall" {
-    name                = "${var.prefix}-sqlfirewall"
+    name                = "${var.PREFIX}-sqlfirewall"
     resource_group_name = azurerm_resource_group.rg.name
     server_name         = azurerm_sql_server.sqlserver.name
     start_ip_address    = "0.0.0.0"
